19f529e00445efeae6e9eb1887efe45def678dac
Gregory Potemkin <potemkin3940@gmail.com>
2023-03-05 23:18:27 +0600

    style

--- backend/db/create_view.py
+++ backend/db/create_view.py
@@ -1,35 +1,40 @@
 import logging
 
 import sqlmodel
-from sqlmodel import Session
+from model import Commit, Repo
 from sqlalchemy import text
-
-from model import Repo, Commit
+from sqlmodel import Session
 
 
 def create_metrics_view(repo: Repo, session: Session):
     successfully_processed_commits = session.exec(
-        sqlmodel.select(Commit)
-        .where(Commit.repo_id == repo.id, Commit.processed == True, Commit.run_ok == True))
+        sqlmodel.select(Commit).where(
+            Commit.repo_id == repo.id, Commit.processed == True, Commit.run_ok == True
+        )
+    )
 
     all_metric_keys = set([])
     for commit in successfully_processed_commits:
         all_metric_keys.update(commit.json_run_result.keys())
 
     if len(all_metric_keys) == 0:
-        logging.debug(f'no metrics found for repo_id={repo.id}')
+        logging.debug(f"no metrics found for repo_id={repo.id}")
         return
 
     json_metric_to_field = [
         f"json_run_result::json->'{m}' as {m}" for m in all_metric_keys
     ]
 
-    stmt = text(f"""create or replace view repo_{repo.id} as select
+    stmt = text(
+        f"""create or replace view repo_{repo.id} as select
                           ref,
                           message,
                           committed_datetime,
-                          """ + ', '.join(json_metric_to_field) + f"""
-                      from commit where repo_id = {repo.id} and processed is true and run_ok is true""")
+                          """
+        + ", ".join(json_metric_to_field)
+        + f"""
+                      from commit where repo_id = {repo.id} and processed is true and run_ok is true"""
+    )
 
     with session.connection().engine.connect() as conn:
         with conn.begin():
