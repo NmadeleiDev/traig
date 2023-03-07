c5cb1e62ba93d6d9322a08978bb63b9a9d29357c
Gregory Potemkin <potemkin3940@gmail.com>
2023-03-07 18:53:43 +0600

    updated client

--- traig_client/traig_client/client.py
+++ traig_client/traig_client/client.py
@@ -3,7 +3,7 @@ import os
 from enum import Enum
 
 import requests
-from exceptions import ClientNotInitialized
+from traig_client.exceptions import ClientNotInitialized
 
 
 class MetricTypeEnum(str, Enum):
